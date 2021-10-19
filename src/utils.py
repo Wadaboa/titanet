ebc1df630a04351e4ff1ae25997b84a940760246
Alessio Falai <falai.alessio@gmail.com>
2021-10-19 19:51:58 +0200

    add embedding visualization function

--- src/utils.py
+++ src/utils.py
@@ -2,7 +2,120 @@ import random
 
 import torch
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.manifold import TSNE
+from sklearn.decomposition import TruncatedSVD
+from scipy.spatial import ConvexHull
+from scipy import interpolate
+
+
+def visualize_embeddings(
+    embeddings,
+    labels,
+    labels_mapping=None,
+    reduction_method="svd",
+    remove_outliers=False,
+    convex_hull=False,
+):
+    """
+    Plot the given embedding vectors, after reducing them to 2D
+    """
+    assert (
+        isinstance(embeddings, np.ndarray)
+        and len(embeddings.shape) == 2
+        and embeddings.shape[1] > 1
+    ), "Wrong embeddings format/dimension"
+    assert (
+        isinstance(labels, np.ndarray)
+        and len(labels.shape) == 1
+        and labels.shape[0] == embeddings.shape[0]
+    ), "Wrong labels format/dimension"
+
+    # Compute dimesionality reduction to 2D
+    if embeddings.shape[1] > 2:
+        embeddings = reduce(
+            embeddings, n_components=2, reduction_method=reduction_method
+        )
+
+    # Store embeddings in a dataframe and compute cluster colors
+    embeddings_df = pd.DataFrame(
+        np.concatenate([embeddings, np.expand_dims(labels, axis=-1)], axis=-1),
+        columns=["x", "y", "l"],
+    )
+    embeddings_df.l = embeddings_df.l.astype(int)
+    cluster_colors = {l: np.random.random(3) for l in np.unique(labels)}
+    embeddings_df["c"] = embeddings_df.l.map(
+        {l: tuple(c) for l, c in cluster_colors.items()}
+    )
+
+    # Plot embeddings and centroids
+    _, ax = plt.subplots()
+    for l, c in cluster_colors.items():
+        to_plot = embeddings_df[embeddings_df.l == l]
+        label = labels_mapping[l] if labels_mapping is not None else l
+        ax.scatter(to_plot.x, to_plot.y, color=c, label=f"{label}")
+        ax.scatter(
+            to_plot.x.mean(),
+            to_plot.y.mean(),
+            color=c,
+            label=f"{label} (C)",
+            marker="^",
+        )
+
+    # Do not represent outliers
+    if remove_outliers:
+        xmin_quantile = np.quantile(embeddings[:, 0], q=0.01)
+        xmax_quantile = np.quantile(embeddings[:, 0], q=0.99)
+        ymin_quantile = np.quantile(embeddings[:, 1], q=0.01)
+        ymax_quantile = np.quantile(embeddings[:, 1], q=0.99)
+        ax.set_xlim(xmin_quantile, xmax_quantile)
+        ax.set_ylim(ymin_quantile, ymax_quantile)
+
+    # Plot a shaded polygon around each cluster
+    if convex_hull:
+        for l, c in cluster_colors.items():
+            try:
+                # Get the convex hull
+                points = embeddings_df[embeddings_df.l == l][["x", "y"]].values
+                hull = ConvexHull(points)
+                x_hull = np.append(
+                    points[hull.vertices, 0], points[hull.vertices, 0][0]
+                )
+                y_hull = np.append(
+                    points[hull.vertices, 1], points[hull.vertices, 1][0]
+                )
+
+                # Interpolate to get a smoother figure
+                dist = np.sqrt(
+                    (x_hull[:-1] - x_hull[1:]) ** 2 + (y_hull[:-1] - y_hull[1:]) ** 2
+                )
+                dist_along = np.concatenate(([0], dist.cumsum()))
+                spline, _ = interpolate.splprep([x_hull, y_hull], u=dist_along, s=0)
+                interp_d = np.linspace(dist_along[0], dist_along[-1], 50)
+                interp_x, interp_y = interpolate.splev(interp_d, spline)
+
+                # Plot the smooth polygon
+                ax.fill(interp_x, interp_y, "--", color=c, alpha=0.2)
+            except:
+                continue
+
+    # Spawn the plot
+    plt.legend()
+    plt.show()
+
+
+def reduce(embeddings, n_components=2, reduction_method="svd", seed=42):
+    """
+    Applies the selected dimensionality reduction technique
+    to the given input data
+    """
+    assert reduction_method in ("svd", "tsne"), "Unsupported reduction method"
+    if reduction_method == "svd":
+        reducer = TruncatedSVD(n_components=n_components, random_state=seed)
+    elif reduction_method == "tsne":
+        reducer = TSNE(n_components=n_components, metric="cosine", random_state=seed)
+    return reducer.fit_transform(embeddings)
 
 
 def plot_spectrogram(spectrogram, figsize=(12, 3)):
