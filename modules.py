9a7f1875e86d22bf882e89474b0900a92e17f512
Alessio Falai <falai.alessio@gmail.com>
2021-10-19 16:17:40 +0200

    add comments/shapes in modules

--- modules.py
+++ modules.py
@@ -1,4 +1,3 @@
-import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
@@ -12,21 +11,44 @@ class Conv1dSamePadding(nn.Conv1d):
     def __init__(self, *args, **kwargs):
         super(Conv1dSamePadding, self).__init__(*args, **kwargs)
 
-    def forward(self, input):
+    def forward(self, inputs):
+        """
+        Given an input of size [B, CI, WI], return an output
+        [B, CO, WO], where WO = [CI + 2P - K - (K - 1) * (D - 1)] / S + 1,
+        by computing P on-the-fly ay forward time
+
+        B: batch size
+        CI: input channels
+        WI: input width
+        CO: output channels
+        WO: output width
+        P: padding
+        K: kernel size
+        D: dilation
+        S: stride
+        """
         padding = (
-            self.stride[0] * (input.shape[-1] - 1)
-            - input.shape[-1]
+            self.stride[0] * (inputs.shape[-1] - 1)
+            - inputs.shape[-1]
             + self.kernel_size[0]
             + (self.dilation[0] - 1) * (self.kernel_size[0] - 1)
         ) // 2
         return self._conv_forward(
-            F.pad(input, (padding, padding)),
+            F.pad(inputs, (padding, padding)),
             self.weight,
             self.bias,
         )
 
 
 class DepthwiseConv1d(nn.Module):
+    """
+    Compute a depth-wise separable convolution, by performing
+    a depth-wise convolution followed by a point-wise convolution
+
+    "Xception: Deep Learning with Depthwise Separable Convolutions",
+    Chollet, https://arxiv.org/abs/1610.02357
+    """
+
     def __init__(
         self,
         in_channels,
@@ -56,8 +78,19 @@ class DepthwiseConv1d(nn.Module):
             ),
         )
 
-    def forward(self, x):
-        return self.conv(x)
+    def forward(self, inputs):
+        """
+        Given an input of size [B, CI, WI], return an output
+        [B, CO, WO], where CO is given as a parameter and WO
+        depends on the convolution operation attributes
+
+        B: batch size
+        CI: input channels
+        WI: input width
+        CO: output channels
+        WO: output width
+        """
+        return self.conv(inputs)
 
 
 class ConvBlock1d(nn.Module):
@@ -82,6 +115,7 @@ class ConvBlock1d(nn.Module):
             "tanh",
         ), "Incompatible activation function"
 
+        # Define architecture
         conv_module = DepthwiseConv1d if depthwise else Conv1dSamePadding
         modules = [
             conv_module(
@@ -97,16 +131,37 @@ class ConvBlock1d(nn.Module):
             modules += [nn.ReLU() if activation == "relu" else nn.Tanh()]
         if dropout > 0:
             modules += [nn.Dropout(p=dropout)]
-
         self.conv_block = nn.Sequential(*modules)
 
-    def forward(self, x):
-        return self.conv_block(x)
+    def forward(self, inputs):
+        """
+        Given an input of size [B, CI, WI], return an output
+        [B, CO, WO], where CO is given as a parameter and WO
+        depends on the convolution operation attributes
+
+        B: batch size
+        CI: input channels
+        WI: input width
+        CO: output channels
+        WO: output width
+        """
+        return self.conv_block(inputs)
 
 
 class SqueezeExcitation(nn.Module):
+    """
+    The SE layer squeezes a sequence of local feature vectors into
+    a single global context vector, broadcasts this context back to
+    each local feature vector, and merges the two via multiplications
+
+    "Squeeze-and-Excitation Networks", Hu et al.,
+    https://arxiv.org/abs/1709.01507
+    """
+
     def __init__(self, channels, reduction=16):
-        super().__init__()
+        super(SqueezeExcitation, self).__init__()
+
+        # Define architecture
         self.squeeze = nn.AdaptiveAvgPool1d(1)
         self.excitation = nn.Sequential(
             nn.Linear(channels, channels // reduction, bias=False),
@@ -115,7 +170,20 @@ class SqueezeExcitation(nn.Module):
             nn.Sigmoid(),
         )
 
-    def forward(self, x):
-        s = self.squeeze(x).squeeze(-1)
-        e = self.excitation(s).unsqueeze(-1)
-        return x * e.expand_as(x)
+    def forward(self, inputs):
+        """
+        Given an input of shape [B, C, W], returns an
+        output of the same shape
+
+        B: batch size
+        C: number of channels
+        W: input width
+        """
+        # [B, C, W] -> [B, C]
+        squeezed = self.squeeze(inputs).squeeze(-1)
+
+        # [B, C] -> [B, C]
+        excited = self.excitation(squeezed).unsqueeze(-1)
+
+        # [B, C] -> [B, C, W]
+        return inputs * excited.expand_as(inputs)
